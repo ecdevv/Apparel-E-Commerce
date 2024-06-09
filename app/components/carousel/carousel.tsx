7437e22aee0941d2c82d5d52c571aa4fd26c800a
ec.dev <ech2447@gmail.com>
2024-06-09 01:26:32 -0500

    mostly CSS edits and slight redesign of front page.

--- app/components/carousel/carousel.tsx
+++ app/components/carousel/carousel.tsx
@@ -26,17 +26,11 @@ const getWrappedImages = (currentIndex:number, images:StaticImageData[], numberO
 const carousel = () => {
   const [buttonDisabled, setButtonDisabled] = useState(false);
   const [currentIndex, setCurrentIndex] = useState(0);
-  const [slideDirection, setSlideDirection] = useState('')
-
-  useEffect(() => {
-    console.log(currentIndex)
-    console.log(slideDirection)
-  }, [currentIndex])
+  const [slideDirection, setSlideDirection] = useState('slide-left');
 
   const handlePrevClick = () => {
+    // Disable and then re-enable the button after 200 milliseconds
     setButtonDisabled(true);
-
-    // Re-enable the button after 00 milliseconds)
     setTimeout(() => {
       setButtonDisabled(false);
     }, 200);
@@ -48,9 +42,8 @@ const carousel = () => {
   }
 
   const handleNextClick = () => {
+    // Disable and then re-enable the button after 200 milliseconds
     setButtonDisabled(true);
-
-    // Re-enable the button after 00 milliseconds)
     setTimeout(() => {
       setButtonDisabled(false);
     }, 200);
@@ -86,22 +79,22 @@ const carousel = () => {
         </CSSTransition>
       </TransitionGroup>
         
-      <button onClick={handlePrevClick} disabled={buttonDisabled}>
+      <button onClick={handlePrevClick} disabled={buttonDisabled} className='carousel-left-btn'>
         <svg
           fill="currentColor"
-          viewBox="0 0 16 16"
+          viewBox="0 0 24 24"
           className='carousel-left-arrow'
         >
-          <path d="M8 0a8 8 0 100 16A8 8 0 008 0zm3.5 7.5a.5.5 0 010 1H5.707l2.147 2.146a.5.5 0 01-.708.708l-3-3a.5.5 0 010-.708l3-3a.5.5 0 11.708.708L5.707 7.5H11.5z" />
+          <path d="M14.2893 5.70708C13.8988 5.31655 13.2657 5.31655 12.8751 5.70708L7.98768 10.5993C7.20729 11.3805 7.2076 12.6463 7.98837 13.427L12.8787 18.3174C13.2693 18.7079 13.9024 18.7079 14.293 18.3174C14.6835 17.9269 14.6835 17.2937 14.293 16.9032L10.1073 12.7175C9.71678 12.327 9.71678 11.6939 10.1073 11.3033L14.2893 7.12129C14.6799 6.73077 14.6799 6.0976 14.2893 5.70708Z"/>
         </svg>
       </button>
-      <button onClick={handleNextClick} disabled={buttonDisabled}>
+      <button onClick={handleNextClick} disabled={buttonDisabled} className='carousel-right-btn'>
         <svg
           fill="currentColor"
-          viewBox="0 0 16 16"
+          viewBox="0 0 24 24"
           className='carousel-right-arrow'
         >
-          <path d="M8 0a8 8 0 110 16A8 8 0 018 0zM4.5 7.5a.5.5 0 000 1h5.793l-2.147 2.146a.5.5 0 00.708.708l3-3a.5.5 0 000-.708l-3-3a.5.5 0 10-.708.708L10.293 7.5H4.5z" />
+          <path d="M9.71069 18.2929C10.1012 18.6834 10.7344 18.6834 11.1249 18.2929L16.0123 13.4006C16.7927 12.6195 16.7924 11.3537 16.0117 10.5729L11.1213 5.68254C10.7308 5.29202 10.0976 5.29202 9.70708 5.68254C9.31655 6.07307 9.31655 6.70623 9.70708 7.09676L13.8927 11.2824C14.2833 11.6729 14.2833 12.3061 13.8927 12.6966L9.71069 16.8787C9.32016 17.2692 9.32016 17.9023 9.71069 18.2929Z"/>
         </svg>
       </button>
     </>
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,16 @@
 import Carousel from './components/carousel/carousel'
+import Link from 'next/link';
 
 export default function Home() {
   return (
     <section className='home'>
-      <div className="carousel-container"><Carousel/></div>
+      <div className="carousel-container">
+        <Carousel/>
+        <div className='slide-content'>
+          <h1 className='slide-h1'>NEW SUMMER COLLECTION</h1>
+          <Link href='/' aria-label='New' className='slide-btn'>Shop Now</Link>
+        </div>
+      </div>
     </section>
   );
 }
